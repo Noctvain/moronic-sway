2255d21032accef384cbf401a45a8e12c2400386
Noctvain <tishyn.v@proton.me>
2025-11-09 15:40:08 +0200

    + tab navigation keymaps

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -8,5 +8,8 @@ return {
   config = function()
     vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
     vim.keymap.set("n", "<C-t>", ":tabnew<CR>", {})
+    vim.keymap.set("n", "<C-Right>", ":tabnext<CR>", {})
+    vim.keymap.set("n", "<C-Left>",  ":tabprevious<CR>", {})
+    vim.keymap.set("n", "<C-w>", ":tabclose<CR>", {})
   end,
 }
